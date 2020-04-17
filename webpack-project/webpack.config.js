be8865ec6d7b9c9d7c76297c796201b9e666cda2
sangTran <sangthien2010@gmail.com>
2020-04-17 13:07:49 -0500

    updating babel and eslint with new plugin

--- webpack-project/webpack.config.js
+++ webpack-project/webpack.config.js
@@ -9,13 +9,20 @@ module.exports = {
     contentBase: "./dist",
   },
   module: {
-      rules: [
-          test: /\.(.js|.jsx)$/,
-          exclude: /node_modules/,
-          use: ['babel-loader']
-      ]
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["eslint-loader"],
+      },
+    ],
   },
   resolve: {
-      extensions: ['js', 'jsx']
-  }
+    extensions: [".js", ".jsx"],
+  },
 };
