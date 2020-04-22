b696d0412a069a585873dd009c275e47785baf20
sangTran <sangthien2010@gmail.com>
2020-04-22 09:53:20 -0500

    adding first new jest tests

--- testing/script.js
+++ testing/script.js
@@ -8,11 +8,13 @@ const googleDatabase = [
   "myfavouritecats2.com",
 ];
 
-const googlSearch = (searchInput) => {
-  const matches = googleDatabase.filter((website) => {
+const googlSearch = (searchInput, db) => {
+  const matches = db.filter((website) => {
     return website.includes(searchInput);
   });
   return matches.length > 3 ? matches.slice(0, 3) : matches;
 };
 
-console.log(googlSearch("cats"));
+// console.log(googlSearch("cats", googleDatabase));
+
+module.exports = googlSearch;
--- testing/script.test.js
+++ testing/script.test.js
@@ -1,1 +1,13 @@
-it("this is a test", () => {});
+const googleSearch = require("./script");
+
+dbMock = ["dog.com", "cheesepuff.com", "disney.com", "dogpictures.com"];
+
+it("this is a test", () => {
+  expect("hello").toBe("hello");
+  googleSearch("testtest", dbMock);
+});
+
+it("is searching google", () => {
+  expect(googleSearch("testtest", dbMock)).toEqual([]);
+  expect(googleSearch("dog", dbMock)).toEqual(["dog.com", "dogpictures.com"]);
+});
