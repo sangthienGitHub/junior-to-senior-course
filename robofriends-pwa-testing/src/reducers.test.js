d14fd9ff78d118af23b68618ee5eccea3426fab4
sangTran <sangthien2010@gmail.com>
2020-04-22 15:57:21 -0500

    add new tests for reducer and fix a bug

--- robofriends-pwa-testing/src/reducers.test.js
+++ robofriends-pwa-testing/src/reducers.test.js
@@ -26,3 +26,61 @@ describe("searchRobots", () => {
     });
   });
 });
+
+describe("requestRobots", () => {
+  const initialStateRobots = {
+    robots: [],
+    isPending: false,
+  };
+  it("should return the initial state", () => {
+    expect(reducers.requestRobots(undefined, {})).toEqual(initialStateRobots);
+  });
+
+  it("should handle REQUEST_ROBOT_PENDING action", () => {
+    expect(
+      reducers.requestRobots(initialStateRobots, {
+        type: REQUEST_ROBOTS_PENDING,
+      })
+    ).toEqual({
+      robots: [],
+      isPending: true,
+    });
+  });
+
+  it("should handle REQUEST_ROBOT_SUCCESS action", () => {
+    expect(
+      reducers.requestRobots(initialStateRobots, {
+        type: REQUEST_ROBOTS_SUCCESS,
+        payload: [
+          {
+            id: "123",
+            name: "test",
+            email: "test@gmail.com",
+          },
+        ],
+      })
+    ).toEqual({
+      robots: [
+        {
+          id: "123",
+          name: "test",
+          email: "test@gmail.com",
+        },
+      ],
+      isPending: false,
+    });
+  });
+
+  it("should handle REQUEST_ROBOT_FAILED action", () => {
+    expect(
+      reducers.requestRobots(initialStateRobots, {
+        type: REQUEST_ROBOTS_FAILED,
+        payload: "NOOOOOO!!!!!!!!",
+      })
+    ).toEqual({
+      error: "NOOOOOO!!!!!!!!",
+      robots: [],
+      isPending: false,
+    });
+  });
+});
