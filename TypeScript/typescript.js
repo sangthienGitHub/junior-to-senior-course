7ea6ba093c0547bb55388be653befaebec9b194c
sangTran <sangthien2010@gmail.com>
2020-04-23 10:37:36 -0500

    adding some new types for typescript

--- TypeScript/typescript.js
+++ TypeScript/typescript.js
@@ -2,4 +2,47 @@ var sum = function (a, b) {
     return a + b;
 };
 var answer = sum(4, 5);
-console.log(answer);
+//boolean
+var isCool = true;
+//number
+var age = 56;
+//string
+var eyeColor = "brown";
+var quote = "I'm not old, i'm only " + age;
+//array
+var pets = ["cat", "dog", "pig"];
+var pets2 = ["lion", "dragon", "lizard"];
+//Object
+var wizard = {
+    a: "John"
+};
+//null and undefined
+var meh = undefined;
+var noo = null;
+//Tuple
+var basket;
+basket = ["basketball", 5];
+//Enum
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
+var sizeName = Size[2];
+var sizeNumber = Size.Small;
+//Any - !! BE CAREFUL, any type is ok with any type
+var whatever = "aghhhh noo!!";
+whatever = 5;
+whatever = true;
+whatever = basket;
+whatever = "hi!!";
+//void --> nothing return
+var sing = function () {
+    console.log("lalalala");
+};
+//never --> not return and doesn't have an end point
+// will have error while replacing void with never previous statement
+var error = function () {
+    throw Error("ooops");
+};
--- TypeScript/typescript.ts
+++ TypeScript/typescript.ts
@@ -4,4 +4,56 @@ const sum = (a: number, b: number) => {
 
 const answer = sum(4, 5);
 
-console.log(answer);
+//boolean
+let isCool: boolean = true;
+
+//number
+let age: number = 56;
+
+//string
+let eyeColor: string = "brown";
+let quote: string = `I'm not old, i'm only ${age}`;
+
+//array
+let pets: string[] = ["cat", "dog", "pig"];
+let pets2: Array<string> = ["lion", "dragon", "lizard"];
+
+//Object
+let wizard: object = {
+  a: "John",
+};
+
+//null and undefined
+let meh: undefined = undefined;
+let noo: null = null;
+
+//Tuple
+let basket: [string, number];
+basket = ["basketball", 5];
+
+//Enum
+enum Size {
+  Small = 1,
+  Medium = 2,
+  Large = 3,
+}
+let sizeName: string = Size[2];
+let sizeNumber: number = Size.Small;
+
+//Any - !! BE CAREFUL, any type is ok with any type
+let whatever: any = "aghhhh noo!!";
+whatever = 5;
+whatever = true;
+whatever = basket;
+whatever = "hi!!";
+
+//void --> nothing return
+let sing = (): void => {
+  console.log("lalalala");
+};
+
+//never --> not return and doesn't have an end point
+// will have error while replacing void with never previous statement
+let error = (): never => {
+  throw Error("ooops");
+};
